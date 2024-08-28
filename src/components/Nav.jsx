02ef63aca26ede3bf33b492d6cc2ea6e4520f7ef
Benjamin Phillips <benjamin.phillips.webdev@gmail.com>
2024-08-28 17:40:11 -0400

    finished nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,23 @@
-import Image from "next/image"
-
 export default function Nav() {
     return (
-        <section className="backdrop-blur-lg backdrop-brightness-75 fixed top-0 left-0 z-10 w-full h-16 py-2 flex flex-row justify-between px-2">
+        <nav className="backdrop-blur-lg backdrop-brightness-75 fixed top-0 left-0 z-10 w-full h-16 py-2 flex flex-row justify-between items-center px-6 border-b border-white/10">
             <img 
-                src="/images/logo-text.png"
-                className="drop-shadow-md"
+                src="/images/logo-text-white.png"
+                className="drop-shadow-md h-full"
+                alt="logo click to scroll to top"
             />
-        </section>
+            <ul className="flex flex-row gap-6 text-white">
+                <li>
+                    <a href="#about-me" className="hover:-top-0.5 top-0 relative border-b border-transparent hover:border-white duration-75">
+                        About Me
+                    </a>
+                </li>
+                <li>
+                    <a href="#contact" className="hover:-top-0.5 top-0 relative border-b border-transparent hover:border-white duration-75">
+                        Contact
+                    </a>
+                </li>
+            </ul>
+        </nav>
     )
 }
